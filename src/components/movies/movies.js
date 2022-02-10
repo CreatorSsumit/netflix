ed1504a4341e5fb66cb70ed9fb5135d35566ab4f
sumit <skkushwaha@netlink.com>
2022-02-10 16:47:20 +0530

    qqc

--- src/components/movies/movies.js
+++ src/components/movies/movies.js
@@ -17,6 +17,8 @@ useEffect(() => {
   
   return (
     <section class="text-gray-600 body-font  bg-dark">
+         <button style={{position:'absolute',top:'5%',left:'2%'}} onClick={()=>navigate('/')} class="ml-4  inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200  text-lg"> <i class="fa-solid fa-arrow-left"></i> </button>
+       
     <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
       <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
         <img  class="rounded mx-auto sm:w-80 w-full"  alt="hero" src={data.url} />
@@ -27,8 +29,7 @@ useEffect(() => {
         </h1>
         <p class="mb-8 text-gray-300 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
         <div class="flex justify-center">
-          <button class="inline-flex text-white bg-indigo-800 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Play</button>
-          <button onClick={()=>navigate('/')} class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Back</button>
+          <button class="inline-flex w-60 text-white bg-indigo-800 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Play <span> <i class="fa-solid fa-circle-play ml-6"></i></span></button>
         </div>
       </div>
     </div>
